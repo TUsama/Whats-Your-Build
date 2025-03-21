8e5859dd6b934fecf60e410c6b14c0da42e8f491
TUsama <1716976420@qq.com>
2025-03-21 15:55:18 +0800

    misc

--- forge/src/main/java/me/clefal/whats_your_build/WhatsYourBuild.java
+++ forge/src/main/java/me/clefal/whats_your_build/WhatsYourBuild.java
@@ -1,24 +1,16 @@
 package me.clefal.whats_your_build;
 
-import me.clefal.whats_your_build.client.keybind.WYBKeys;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-import java.util.Arrays;
-
 @Mod(Constants.MOD_ID)
 public class WhatsYourBuild {
-    
-    public WhatsYourBuild() {
-        /*
-        CommonClass.init();
 
+    public WhatsYourBuild() {
 
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> new DistExecutor.SafeRunnable() {
             @Override
@@ -29,8 +21,8 @@ public class WhatsYourBuild {
                 WhatsYourBuildForgeClient.eventInit(modEventBus, eventBus);
             }
         });
-        CommonClass.serverInit();*/
-
+        CommonClass.serverInit();
+        CommonClass.packetInit();
 
 
     }
