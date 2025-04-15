60fc8a6ba4f2527434690a1e87bbb8f1422f2638
TUsama <1716976420@qq.com>
2025-04-15 21:05:20 +0800

    implement BuildClickEvent

---
+++ common/src/main/java/me/clefal/whats_your_build/mixin/ScreenMixin.java
@@ -0,0 +1,38 @@
+package me.clefal.whats_your_build.mixin;
+
+import com.clefal.nirvana_lib.utils.NetworkUtils;
+import me.clefal.whats_your_build.chat.BuildClickEvent;
+import me.clefal.whats_your_build.network.c2s.C2SAskBuildPacket;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.UUID;
+
+@Mixin(value = Screen.class)
+public class ScreenMixin {
+    @Inject(
+            method = "handleComponentClicked", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/network/chat/ClickEvent;getAction()Lnet/minecraft/network/chat/ClickEvent$Action;"
+    ),
+            cancellable = true)
+    public void onClickBuildClickEvent(Style style, CallbackInfoReturnable<Boolean> cir) {
+        ClickEvent clickEvent = style.getClickEvent();
+        if (clickEvent instanceof BuildClickEvent buildClickEvent) {
+            UUID uuid = UUID.fromString(buildClickEvent.getValue());
+            Player playerByUUID = Minecraft.getInstance().level.getPlayerByUUID(uuid);
+            if (playerByUUID != null) {
+                NetworkUtils.sendToServer(new C2SAskBuildPacket(uuid, true));
+            }
+            cir.setReturnValue(false);
+        }
+
+    }
+}
